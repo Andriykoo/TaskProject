eea92b91408601f7640788c2f88587764ec2ed10
Andriykoo <Andriykoo>
2015-07-30 19:27:07 +0300

    Add admini in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name:"Admin",email:"admin@gmail.com",password:"12345678")
+User.first.add_role :admin
